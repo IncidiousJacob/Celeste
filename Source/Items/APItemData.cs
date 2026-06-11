a93851504c35e6bdf5417c331975a0f026d972e4
Incidious <jacobdock9@gmail.com>
2026-06-11 10:25:53 -0600

    Add new fruit items to ItemIDToString dictionary

--- Source/Items/APItemData.cs
+++ Source/Items/APItemData.cs
@@ -66,6 +66,10 @@ namespace Celeste.Mod.Celeste_Multiworld.Items
         {
             { 0xCA10000, "Strawberry" },
             { 0xCA10001, "Raspberry" },
+            { 0xCA10000, "Blueberry" },
+            { 0xCA10001, "Blackberry" },
+            { 0xCA10000, "Banana" },
+            { 0xCA10001, "Saskatoon Berry" },
 
             { 0xCA10010, "Granny's House Keys" },
 
